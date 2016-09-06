8a941a5d4420d60d5ca3457dd72b155e5975366c
Erayd <steve@erayd.net>
2016-09-06 23:43:15 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,94 @@
+Spique - A Spiral Double-Ended Queue
+====================================
+
+Spique is a deque implemented as a doubly-linked list of circular buffers. This
+structure allows for both high performance and unlimited dynamic growth of the
+queue.
+
+Spique does not require an initial or maximum size (although you can define a
+maximum if you wish), and will both grow and shrink dynamically as items are
+added and removed.
+
+## API
+```javascript
+var s = new Spique(maxItems, ringSize);
+```
+`maxItems` sets the maximum number of items which may be stored in the queue at
+any given time. Attempting to store more items than this will throw an error. If
+maxItems is falsy, then there is no maximum, and the queue may continue to grow
+as long as there is available memory. By default, `maxItems` is unlimited.
+
+`ringSize` sets the number of items stored in each ring. This defaults to 1000,
+which is normally fine for most purposes. Dynamic resizing is done in chunks of
+`ringSize` items (i.e. one buffer at a time).
+
+Both `maxItems` and `ringSize` are optional.
+
+Spique can also be used as an iterator - this pattern will call dequeue() until
+the queue is empty.
+```javascript
+for(myValue of s) {
+  doSomething(myValue);
+}
+```
+
+### .push(), .enqueue()
+```javascript
+s.push(myValue);
+s.enqueue(myValue);
+```
+Append a value to the end of the queue. If a max queue size has been set, and the
+queue is full, then this method will throw an error. `enqueue()` and `push()` are
+synonymous.
+
+### .shift(), .dequeue()
+```javascript
+var myValue = s.shift();
+var myValue = s.dequeue();
+```
+Return the value at the head of the queue, and remove it from the queue. If the
+queue is empty, this method will throw an error. `dequeue()` and `shift()` are
+synonymous.
+
+### .unshift()
+```javascript
+s.unshift(myValue);
+```
+Prepend a value to the head of the queue. If a max queue size has been set, and
+the queue is full, then this method will throw an error.
+
+### .pop()
+```javascript
+var myValue = s.pop();
+```
+Return the value at the end of the queue, and remove it from the queue. If the
+queue is empty, then this method will throw an error.
+
+### .peek()
+```javascript
+var myValue = s.peek();
+```
+Return the value at the end of the queue. The value is not removed. If the queue
+is empty, then this method will throw an error.
+
+### .peekStart()
+```javascript
+var myValue = s.peekStart();
+```
+Return the value at the head of the queue. The value is not removed. If the queue
+is empty, then this method will throw an error.
+
+### Properties
+#### .length
+The number of items currently stored in the queue.
+
+#### .capacity
+The current capacity of the queue - this will grow as items are inserted.
+
+#### .maxItems
+The maximum number of items allowed in the buffer at any given time. If this is
+unlimited, then maxItems will be zero.
+
+#### .ringSize
+The size of each circular buffer. The queue will grow / shrink by this many items
+at a time.
---
+++ spique.js
@@ -0,0 +1,209 @@
+/*                                 ISC License
+ *
+ * Copyright (c) 2016, Erayd LTD
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
+ * OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+module.exports = Spique;
+
+function Spique(maxItems, circumference) {
+  var defaultCircumference = 1000;
+  circumference = circumference ? parseInt(circumference) : defaultCircumference;
+
+  var firstRing = new RingBuffer(circumference);
+  var lastRing = firstRing;
+  var items = 0;
+
+  // check whether the buffer is empty
+  this.isEmpty = function() {
+    return firstRing == lastRing && firstRing.isEmpty();
+  };
+
+  // push item(s) onto the end of the buffer
+  this.enqueue = this.push = function() {
+    for(value of arguments) {
+      if(items >= maxItems)
+        throw new Error('Buffer is full');
+      // add another ring if necessary
+      if(!lastRing.available()) {
+        var newRing = new RingBuffer(circumference);
+        lastRing.nextRing = newRing;
+        newRing.prevRing = lastRing;
+        lastRing = newRing;
+      }
+      lastRing.push(value);
+      items++;
+    }
+  }
+
+  // push item(s) onto the start of the buffer
+  this.unshift = function(value) {
+    for(value of arguments) {
+      if(items >= maxItems)
+        throw new Error('Buffer is full');
+      // add another ring if necessary
+      if(!firstRing.available()) {
+        newRing = new RingBuffer(circumference);
+        newRing.nextRing = firstRing;
+        firstRing.prevRing = newRing;
+        firstRing = newRing;
+      }
+      firstRing.unshift(value);
+      items++;
+    }
+  }
+
+  // pop an item off the end of the buffer
+  this.pop = function() {
+    var value = lastRing.pop();
+    // delete the ring if it's empty and not the last one
+    if(lastRing.isEmpty() && lastRing.prevRing) {
+      lastRing = lastRing.prevRing;
+      delete lastRing.nextRing;
+    }
+    items--;
+    return value;
+  };
+
+  // pop an item off the start of the buffer
+  this.dequeue = this.shift = function() {
+    var value = firstRing.shift();
+    // delete the ring if it's empty and not the last one
+    if(firstRing.isEmpty() && firstRing.nextRing) {
+      firstRing = firstRing.nextRing;
+      delete firstRing.prevRing;
+    }
+    items--;
+    return value;
+  };
+
+  // peek at the end of the buffer
+  this.peek = function() {
+    return lastRing.peek();
+  };
+
+  // peek at the start of the buffer
+  this.peekStart = function() {
+    return firstRing.peekStart();
+  };
+
+  // iterator dequeue
+  this[Symbol.iterator] = function*() {
+    while(!this.isEmpty())
+      yield this.dequeue();
+  };
+
+  // get the number of items in the buffer
+  Object.defineProperty(this, 'length', {get: function() {
+    var length = 0;
+    for(var r = firstRing; r; r = r.nextRing)
+      length += r.length;
+    return length;
+  }});
+
+  // get the current capacity
+  Object.defineProperty(this, 'capacity', {get: function() {
+    var capacity = 0;
+    for(var r = firstRing; r; r = r.nextRing)
+      capacity += circumference;
+    return capacity;
+  }});
+
+  // get the max number of items
+  Object.defineProperty(this, 'maxItems', {get: function() {
+    return maxItems | 0;
+  }});
+
+  // get the ring size
+  Object.defineProperty(this, 'ringSize', {get: function() {
+    return circumference;
+  }});
+}
+
+function RingBuffer(size) {
+  var head = 0;
+  var items = 0;
+  var buffer = new Array(size);
+
+  // mark the buffer as empty and optionally unset all the slots
+  this.empty = function(unset) {
+    head = 0;
+    if(unset) {
+      for(var i = size; i;)
+        delete buffer[--i];
+    }
+  }
+
+  // check whether the buffer is empty
+  this.isEmpty = function() {
+    return !items;
+  }
+
+  // get the number of free slots
+  this.available = function() {
+    return size - items;
+  }
+
+  // push item onto the end of the buffer
+  this.push = function(value) {
+    var pos = (head + items++) % size;
+    buffer[pos] = value;
+  };
+
+  // push item onto the start of the buffer
+  this.unshift = function(value) {
+    var pos = head ? --head : (head = size - 1);
+    buffer[pos] = value;
+    items++;
+  };
+
+  // pop an item off the end of the buffer
+  this.pop = function() {
+    if(this.isEmpty())
+      throw new Error('Buffer is empty');
+    var pos = (head + --items) % size;
+    var value = buffer[pos];
+    delete buffer[pos];
+    return value;
+  };
+
+  // pop an item off the start of the buffer
+  this.shift = function() {
+    if(this.isEmpty())
+      throw new Error('Buffer is empty');
+    var value = buffer[head];
+    delete buffer[head];
+    if(++head == size)
+      head = 0;
+    items--;
+    return value;
+  };
+
+  // peek at the end of the buffer
+  this.peek = function() {
+    if(this.isEmpty())
+      throw new Error('Buffer is empty');
+    return buffer[(head + (items - 1)) % size];
+  };
+
+  // peek at the start of the buffer
+  this.peekStart = function() {
+    if(this.isEmpty())
+      throw new Error('Buffer is empty');
+    return buffer[head];
+  };
+
+  // get the number of items in the buffer
+  Object.defineProperty(this, 'length', {get: function() {
+    return items;
+  }});
+}
