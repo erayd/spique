cce09f52a6ca300063b218e2f14cd39829d509d0
Steve Gilberd <steve@erayd.net>
2020-01-08 12:16:36 +1300

    Refactor with core functionality

--- spique.js
+++ spique.js
@@ -15,378 +15,152 @@
  */
 
 "use strict";
-const events = require("events");
+const EventEmitter = require("events").EventEmitter;
 const RingBuffer = require("./ringbuffer.js");
-const GeneratorFunction = function*() {}.constructor;
 
-module.exports = class Spique extends events.EventEmitter {
-    constructor(maxItems, ringSize, _async = true) {
+module.exports = class Spique extends EventEmitter {
+    constructor(size = 0, ringSize = 1024) {
         super();
-        var defaultRingSize = 1000;
-        ringSize = ringSize ? parseInt(ringSize) : defaultRingSize;
-        if (!maxItems || !(typeof maxItems === "number"))
-            maxItems = Math.floor(Number.MAX_VALUE);
 
-        var firstRing = allocateRing();
-        var lastRing = firstRing;
-        var spareRing = undefined;
+        var headRing = new RingBuffer(ringSize);
+        var tailRing = headRing;
         var rings = 1;
         var items = 0;
-        var lifetimeIn = 0;
-        var lifetimeOut = 0;
-        var ttl = 0;
-        var pending = _async ? new Spique(null, null, false) : null;
-        var closed = false;
 
-        // fill from provided iterables
-        var fillFrom = _async ? new Spique(null, null, false) : null;
-        var doFillFrom = () => {
-            if (fillFrom) {
-                while (!fillFrom.isEmpty() && !this.isFull()) {
-                    let v = fillFrom.first().source.next();
-                    if (v.done) {
-                        fillFrom.dequeue();
-                    } else {
-                        this[fillFrom.first().unshift ? "unshift" : "enqueue"](v.value);
-                    }
-                }
-            }
-        };
+        // basic properties
+        Object.defineProperties(this, {
+            // get the size of the queue
+            size: { value: size, writable: false, enumerable: true },
 
-        // immediately call newly attached event handlers if appropriate
+            // get the current number of items in the buffer
+            length: { get: () => items, enumerable: true },
+
+            // get the current number of free slots in the buffer
+            free: {
+                get: () => (size ? size - items : Number.MAX_SAFE_INTEGER),
+                enumerable: true
+            },
+
+            // methods
+            ringSize: { value: ringSize, writable: false, enumerable: true },
+            enqueue: { value: enqueue, writable: false },
+            enqueueHead: { value: enqueueHead, writable: false },
+            dequeue: { value: dequeue, writable: false },
+            dequeueTail: { value: dequeueTail, writable: false },
+            peek: { value: peek, writable: false },
+            peekTail: { value: peekTail, writable: false }
+        });
+
+        // call listener if event state is valid on attachment
         this.on("newListener", (ev, listener) => {
             if (
-                (ev === "ready" && !this.isEmpty()) ||
-                (ev === "full" && this.isFull()) ||
-                (ev === "empty" && this.isEmpty()) ||
-                (ev === "space" && !this.isFull()) ||
-                (ev === "close" && this.isClosed() && this.isEmpty()) ||
-                (ev === "ttl-in" && ttl && lifetimeIn === ttl) ||
-                (ev === "ttl-out" && ttl && lifetimeOut === ttl)
+                (ev === "data" && items) ||
+                (ev === "empty" && !items) ||
+                (ev === "full" && !this.free) ||
+                (ev === "free" && this.free)
             ) {
                 listener(this);
-            }
+            } else throw new Error(`Invalid event: ${ev}`);
         });
 
-        // allocate a new ring, or return the spare if available
-        function allocateRing() {
-            var newRing = spareRing;
-            if (newRing !== undefined) spareRing = undefined;
-            else newRing = new RingBuffer(ringSize);
-            return newRing;
+        // add an item to the tail of the queue
+        function enqueue(value) {
+            if (!this.free) throw new Error("Queue is full");
+
+            // allocate a new ring
+            if (!tailRing.free) {
+                let r = tailRing._below || new RingBuffer(ringSize);
+                r._above = tailRing;
+                tailRing._below = r;
+                tailRing = r;
+                rings++;
+            }
+
+            // enqueue data
+            tailRing.push(value);
+            ++items;
+
+            // fire events
+            if (!this.free) this.emit("full", this);
+            if (items === 1) this.emit("data", this);
         }
 
-        // mark the buffer as closed
-        this.close = function(newTTL = 0) {
-            if (newTTL) {
-                ttl = newTTL;
-                if (ttl < lifetimeIn) {
-                    throw new Error("Attempted to set TTL lower than lifetime inserts");
-                } else {
-                    if (ttl === lifetimeIn) {
-                        this.emit("ttl-in", this);
-                        this.close();
-                    }
-                    if (ttl === lifetimeOut) {
-                        this.emit("ttl-out", this);
-                    }
-                }
-            } else if (!closed) {
-                closed = true;
-            }
-            if (closed && this.isEmpty()) {
-                this.emit("close", this);
-            }
-        };
+        // add an item to the head of the queue
+        function enqueueHead(value) {
+            if (!this.free) throw new Error("Queue is full");
 
-        // check whether the buffer is closed
-        this.isClosed = function() {
-            return closed;
-        };
-
-        // check whether the buffer is empty
-        this.isEmpty = function() {
-            return !items;
-        };
-
-        // check whether the buffer is full
-        this.isFull = function() {
-            return items == maxItems;
-        };
-
-        // push item(s) onto the end of the buffer
-        this.enqueue = this.push = (...values) => {
-            if (closed) throw new Error("Buffer is closed");
-            if (items >= maxItems) throw new Error("Buffer is full");
-            // add another ring if necessary
-            if (!lastRing.free) {
-                var newRing = allocateRing();
-                lastRing.nextRing = newRing;
-                newRing.prevRing = lastRing;
-                lastRing = newRing;
+            // allocate a new ring
+            if (!headRing.free) {
+                let r = headRing._above || new RingBuffer(ringSize);
+                r._below = headRing;
+                headRing._above = r;
+                headRing = r;
                 rings++;
             }
-            lastRing.push(values.shift());
-            let ready = !items++;
 
-            // ttl & lifetime counters
-            if (ttl && ttl === ++lifetimeIn) {
-                this.close();
-                this.emit("ttl-in", this);
-            }
+            // enqueue data
+            headRing.unshift(value);
+            ++items;
 
-            if (values.length) {
-                // unroll variadic args
-                this.push(...values);
-            } else {
-                // fire post-insert events
-                if (items === maxItems) this.emit("full", this);
-            }
-            if (ready) {
-                this.emit("ready", this);
-            }
-        };
+            // fire events
+            if (!this.free) this.emit("full", this);
+            if (items === 1) this.emit("data", this);
+        }
 
-        // push item(s) onto the end of the buffer when there is space available
-        this.enqueueAsync = this.pushAsync = function pushAsync(value) {
-            if (!this.isFull()) {
-                return Promise.resolve(this.push(value));
-            } else {
-                pending.push(() => {
-                    if (closed) return Promise.reject(new Error("Buffer is closed"));
-                    else return Promise.resolve(this.push(value));
-                });
-            }
-        };
+        // peek at the value at the head of the queue
+        function peek() {
+            if (!items) throw new Error("Queue is empty");
 
-        // push item(s) onto the start of the buffer
-        this.unshift = (...values) => {
-            if (closed) throw new Error("Buffer is closed");
-            if (items >= maxItems) throw new Error("Buffer is full");
-            // add another ring if necessary
-            if (!firstRing.free) {
-                var newRing = allocateRing();
-                newRing.nextRing = firstRing;
-                firstRing.prevRing = newRing;
-                firstRing = newRing;
-                rings++;
-            }
-            firstRing.unshift(values.shift());
-            let ready = !items++;
+            return headRing.peekStart();
+        }
 
-            // ttl & lifetime counters
-            if (ttl && ttl === ++lifetimeIn) {
-                this.close();
-                this.emit("ttl-in", this);
-            }
+        // peek at the value at the head of the queue
+        function peekTail() {
+            if (!items) throw new Error("Queue is empty");
 
-            if (values.length) {
-                // unroll variadic args
-                this.unshift(...values);
-            } else {
-                // fire post-insert events
-                if (items === maxItems) this.emit("full", this);
-            }
-            if (ready) {
-                this.emit("ready", this);
-            }
-        };
+            return tailRing.peek();
+        }
 
-        // push item(s) onto the start of the buffer when there is space available
-        this.unshiftAsync = function unshiftAsync(value) {
-            if (!this.isFull()) {
-                return Promise.resolve(this.unshift(value));
-            } else {
-                pending.push(() => {
-                    if (closed) return Promise.reject(new Error("Buffer is closed"));
-                    else return Promise.resolve(this.unshift(value));
-                });
-            }
-        };
+        // remove an item from the head of the queue
+        function dequeue() {
+            if (!items) throw new Error("Queue is empty");
 
-        // pop an item off the end of the buffer
-        this.pop = function() {
-            if (lastRing.isEmpty()) return undefined;
-            var value = lastRing.pop();
-            // delete the ring if it's empty and not the last one
-            if (lastRing.isEmpty() && lastRing.prevRing) {
-                lastRing = lastRing.prevRing;
-                spareRing = lastRing.nextRing;
-                delete lastRing.nextRing;
+            let value = headRing.shift();
+            --items;
+
+            // deallocate unused buffer
+            if (!headRing.length && rings > 1) {
+                headRing._above = undefined;
+                headRing = headRing._below;
                 rings--;
             }
-            items--;
 
-            // ttl & lifetime counters
-            if (ttl && ttl === ++lifetimeOut) {
-                this.emit("ttl-out", this);
-            }
-
-            // autofill
-            doFillFrom();
-
-            // events
-            if (items === 0) {
-                this.emit("empty", this);
-                if (closed) {
-                    this.emit("close", this);
-                }
-            }
-            if (items < maxItems) {
-                while (pending && !this.isFull() && !pending.isEmpty()) pending.shift()();
-                if (items < maxItems && !closed) this.emit("space", this);
-            }
-            if (this.items === 0) this.emit("close", this);
+            // fire events
+            if (!items) this.emit("empty", this);
+            if (this.free) this.emit("free", this);
 
             return value;
-        };
+        }
 
-        // pop an item off the start of the buffer
-        this.dequeue = this.shift = function() {
-            if (firstRing.isEmpty()) return undefined;
-            var value = firstRing.shift();
-            // delete the ring if it's empty and not the last one
-            if (firstRing.isEmpty() && firstRing.nextRing) {
-                firstRing = firstRing.nextRing;
-                spareRing = firstRing.prevRing;
-                delete firstRing.prevRing;
+        // remove an item from the tail of the queue
+        function dequeueTail() {
+            if (!items) throw new Error("Queue is empty");
+
+            let value = tailRing.pop();
+            --items;
+
+            // deallocate unused buffer
+            if (!tailRing.length && rings > 1) {
+                tailRing._below = undefined;
+                tailRing = tailRing._above;
                 rings--;
             }
-            items--;
 
-            // ttl & lifetime counters
-            if (ttl && ttl === ++lifetimeOut) {
-                this.emit("ttl-out", this);
-            }
-
-            // autofill
-            doFillFrom();
-
-            // events
-            if (items === 0) {
-                this.emit("empty", this);
-                if (closed) {
-                    this.emit("close", this);
-                }
-            }
-            if (items < maxItems) {
-                while (pending && !this.isFull() && !pending.isEmpty()) pending.shift()();
-                if (items < maxItems && !closed) this.emit("space", this);
-            }
-            if (this.items === 0) this.emit("close", this);
+            // fire events
+            if (!items) this.emit("empty", this);
+            if (this.free) this.emit("free", this);
 
             return value;
-        };
-
-        // peek at the end of the buffer
-        this.last = this.peek = function() {
-            return lastRing.peek();
-        };
-
-        // peek at the start of the buffer
-        this.first = this.peekStart = function() {
-            return firstRing.peekStart();
-        };
-
-        // fill from a provided iterator or generator
-        this.fillFrom = function(source, unshift = false) {
-            fillFrom.enqueue({ source, unshift });
-            doFillFrom();
-        };
-
-        // apply a transform function and return a new queue of transformed items
-        this.apply = function(transform, reverse = false, ...createParams) {
-            let dest = new Spique(...createParams);
-            let close = false;
-            this.on("close", () => (close = true));
-
-            if (!(transform instanceof GeneratorFunction)) {
-                let plainTransform = transform;
-                transform = function*(item) {
-                    yield plainTransform(item);
-                };
-            }
-
-            let results = (function*() {})();
-            let feed = () => {
-                while (!dest.isFull()) {
-                    let next = results.next();
-                    if (next.done) {
-                        if (!this.isEmpty()) {
-                            results = transform(reverse ? this.pop() : this.dequeue());
-                            continue;
-                        } else {
-                            break;
-                        }
-                    }
-                    dest[reverse ? "unshift" : "enqueue"](next.value);
-                }
-                if (dest.isFull()) {
-                    dest.once("space", feed);
-                } else if (close) {
-                    dest.close();
-                } else {
-                    this.once("ready", feed);
-                }
-            };
-
-            dest.once("space", feed);
-
-            return dest;
-        };
-
-        // iterator dequeue
-        this[Symbol.iterator] = function*() {
-            while (!this.isEmpty()) yield this.dequeue();
-        };
-
-        // get the number of items in the buffer
-        Object.defineProperty(this, "length", {
-            get: function() {
-                return items;
-            }
-        });
-
-        // get the current capacity
-        Object.defineProperty(this, "capacity", {
-            get: function() {
-                return rings * ringSize;
-            }
-        });
-
-        // get the max number of items
-        Object.defineProperty(this, "maxItems", {
-            get: function() {
-                return maxItems | 0;
-            }
-        });
-
-        // get the ring size
-        Object.defineProperty(this, "ringSize", {
-            get: function() {
-                return ringSize;
-            }
-        });
-
-        // get lifetime inserts
-        Object.defineProperty(this, "lifetimeIn", {
-            get: function() {
-                return lifetimeIn;
-            }
-        });
-
-        // get lifetime removes
-        Object.defineProperty(this, "lifetimeOut", {
-            get: function() {
-                return lifetimeOut;
-            }
-        });
-
-        // get ttl
-        Object.defineProperty(this, "ttl", {
-            get: function() {
-                return ttl;
-            }
-        });
+        }
     }
 };
